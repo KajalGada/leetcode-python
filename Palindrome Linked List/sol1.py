f3da99753f0c1e34435ac3be1fcc23465a3fb96f
Kajal Damji Gada <16885692+KajalGada@users.noreply.github.com>
2021-12-25 11:21:01 -0800

    Create sol1.py

---
+++ Palindrome Linked List/sol1.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        nums = []
+        
+        while head: 
+            nums.append(head.val)
+            head = head.next
+            
+        ans = True
+
+        if nums != nums[::-1]:
+            ans = False
+            
+            
+        return ans
